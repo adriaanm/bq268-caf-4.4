ccf1a138685c4d0a87632ac871e0845bac35f6bb
Adriaan Moors <adriaanm@gmail.com>
2026-03-20 20:48:03 +0000

    Revert "audio: register codec on platform device + add ramoops"
    This reverts commit b5f8d985b184752bfc849e44b03dcfb046f04fb7.

--- sound/soc/codecs/msm8x16-wcd.c
+++ sound/soc/codecs/msm8x16-wcd.c
@@ -270,7 +270,6 @@ static struct wcd_mbhc_register
 
 struct msm8x16_wcd_spmi {
 	struct spmi_device *spmi;
-	struct platform_device *pdev;
 	int base;
 };
 
@@ -1261,14 +1260,14 @@ err:
 	return ret;
 }
 
-static int __maybe_unused msm8x16_wcd_volatile(struct snd_soc_codec *codec, unsigned int reg)
+static int msm8x16_wcd_volatile(struct snd_soc_codec *codec, unsigned int reg)
 {
 	dev_dbg(codec->dev, "%s: reg 0x%x\n", __func__, reg);
 
 	return msm8x16_wcd_reg_readonly[reg];
 }
 
-static int __maybe_unused msm8x16_wcd_readable(struct snd_soc_codec *ssc, unsigned int reg)
+static int msm8x16_wcd_readable(struct snd_soc_codec *ssc, unsigned int reg)
 {
 	return msm8x16_wcd_reg_readable[reg];
 }
@@ -6177,7 +6176,6 @@ static int msm8x16_wcd_spmi_probe(struct platform_device *pdev)
 	switch (wcd_resource->start) {
 	case TOMBAK_CORE_0_SPMI_ADDR:
 		msm8x16_wcd_modules[0].spmi = parent_spmi;
-		msm8x16_wcd_modules[0].pdev = pdev;
 		msm8x16_wcd_modules[0].base = (parent_spmi->usid << 16) +
 						wcd_resource->start;
 		wcd9xxx_spmi_set_dev(msm8x16_wcd_modules[0].spmi, 0);
@@ -6186,7 +6184,6 @@ static int msm8x16_wcd_spmi_probe(struct platform_device *pdev)
 		break;
 	case TOMBAK_CORE_1_SPMI_ADDR:
 		msm8x16_wcd_modules[1].spmi = parent_spmi;
-		msm8x16_wcd_modules[1].pdev = pdev;
 		msm8x16_wcd_modules[1].base = (parent_spmi->usid << 16) +
 						wcd_resource->start;
 		wcd9xxx_spmi_set_dev(msm8x16_wcd_modules[1].spmi, 1);
@@ -6264,9 +6261,9 @@ static int msm8x16_wcd_spmi_probe(struct platform_device *pdev)
 	spmi_dev_registered_cnt++;
 register_codec:
 	if ((spmi_dev_registered_cnt == MAX_MSM8X16_WCD_DEVICE) && (!ret)) {
-		if (msm8x16_wcd_modules[0].pdev) {
+		if (msm8x16_wcd_modules[0].spmi) {
 			ret = snd_soc_register_codec(
-					&msm8x16_wcd_modules[0].pdev->dev,
+					&msm8x16_wcd_modules[0].spmi->dev,
 					&soc_codec_dev_msm8x16_wcd,
 					msm8x16_wcd_i2s_dai,
 					ARRAY_SIZE(msm8x16_wcd_i2s_dai));
